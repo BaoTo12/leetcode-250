904df84d1bb3540cbbd15a5be2a15d43771a6631
ChiBao <baotochi87@gmail.com>
2025-08-06 05:59:06 +0700

    solve concentenation of array

---
+++ array-hashing/concatenation-array.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	s1 := []int{1, 2, 1}
+	s2 := getConcatenation(s1)
+	fmt.Println(s2)
+}
+
+func getConcatenation(nums []int) []int {
+	s2 := make([]int, len(nums)*2)
+	for i, v := range nums {
+		s2[i] = v
+		s2[i+len(nums)] = v
+	}
+	return s2
+}
