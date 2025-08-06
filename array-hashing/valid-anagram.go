e656d0b0326d939d89c5155c7b7d1b1cbbbb0b8b
ChiBao <baotochi87@gmail.com>
2025-08-06 07:49:02 +0700

    solve is anagram problem

---
+++ array-hashing/valid-anagram.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func isAnagramDemo() {
+	// s := string("anagram")
+	// t := string("nagaram")
+
+	s := "rat"
+	t := "car"
+	fmt.Println(isAnagram(s, t))
+}
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	s1 := make(map[int32]int, len(s))
+	for _, v := range s {
+		s1[v] += 1
+
+	}
+	t1 := make(map[int32]int, len(t))
+	for _, v := range t {
+		t1[v] += 1
+	}
+
+	for _, v := range s {
+		if s1[v] != t1[v] {
+			return false
+		}
+	}
+	return true
+}
