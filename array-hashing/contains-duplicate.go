fcfa22241493af7762e09c98ffdc17a7411561cc
ChiBao <baotochi87@gmail.com>
2025-08-06 07:28:40 +0700

    add more version of contains duplicate

--- array-hashing/contains-duplicate.go
+++ array-hashing/contains-duplicate.go
@@ -7,7 +7,8 @@ import (
 func ContainsDuplicateDemo() {
 	s := []int{1, 2, 3, 1}
 	// s1 := []int{1, 2, 3, 4}
-	fmt.Println(containsDuplicate(s))
+	fmt.Println(CheckLengthToCheckDuplicate(s))
+	// fmt.Println(CheckLengthToCheckDuplicate(s1))
 }
 
 func containsDuplicate(nums []int) bool {
@@ -19,4 +20,11 @@ func containsDuplicate(nums []int) bool {
 		seen[v] = struct{}{}
 	}
 	return false
+}
+func CheckLengthToCheckDuplicate(nums []int) bool {
+	seen := make(map[int]struct{}, len(nums))
+	for _, v := range nums {
+		seen[v] = struct{}{}
+	}
+	return len(seen) < len(nums)
 }
