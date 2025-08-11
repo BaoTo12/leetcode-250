642b28d4ced003168da296f6dbbdd54befd2f399
ChiBao <baotochi87@gmail.com>
2025-08-11 08:00:52 +0700

    solve group anagrams

--- array-hashing/group-anagram.go
+++ array-hashing/group-anagram.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
-//Input: strs = ["eat","tea","tan","ate","nat","bat"]
-//Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 func GroupAnagramsDemo() {
 	strs := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
 	result := groupAnagrams(strs)
@@ -11,6 +15,29 @@ func GroupAnagramsDemo() {
 }
 
 func groupAnagrams(strs []string) [][]string {
+	if len(strs) == 0 || len(strs) == 1 {
+		return [][]string{strs[:]}
+	}
+	groupAnagrams := map[string][]string{}
+	// TODO Step1: Loop through strs
+	for _, s := range strs {
+		// TODO Step2: For each string we will sort the string to get the sorted key and put them into a slice
+		// get sorted key
+		sliceOfStr := strings.Split(s, "")
+		sort.StringSlice(sliceOfStr).Sort()
+		sortedKey := strings.Join(sliceOfStr, "")
 
-	return nil
+		// add sorted key to groupAnagrams
+		if _, err := groupAnagrams[sortedKey]; !err {
+			groupAnagrams[sortedKey] = []string{}
+		}
+		groupAnagrams[sortedKey] = append(groupAnagrams[sortedKey], s)
+	}
+	fmt.Println(groupAnagrams)
+	// TODO Step3: Return the slice of group anagram
+	result := [][]string{}
+	for _, ana := range groupAnagrams {
+		result = append(result, ana)
+	}
+	return result
 }
